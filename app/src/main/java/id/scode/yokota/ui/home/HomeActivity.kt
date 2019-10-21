2554cd52fd25908570d2dbe1b86931266959d6b1
yogithesymbian <yogirenbox33@gmail.com>
2019-10-21 13:52:29 +0800

    init-design

--- app/src/main/java/id/scode/yokota/ui/home/HomeActivity.kt
+++ app/src/main/java/id/scode/yokota/ui/home/HomeActivity.kt
@@ -1,13 +1,20 @@
 package id.scode.yokota.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.scode.yokota.R
+import id.scode.yokota.ui.auth.RegisterActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btn_register.setOnClickListener{
+            startActivity(Intent(this@HomeActivity, RegisterActivity::class.java))
+        }
     }
 }
